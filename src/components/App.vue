998f54835fe9fb984a5e38826864ff3cf0cadd3e
Marco A <mabo.marcoantonio@gmail.com>
2019-06-07 19:42:26 -0300

    Modify - Time to build

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>Hello World - Welcome to my website - {{ timer }}</h1>
+    <h1>Welcome to my website</h1>
+    <h2>My time to build the website:</h2>
+    <h3>Days: {{ days }} Hours: {{ hours }} Minutes: {{ minutes }} Seconds: {{ seconds }}</h3>
   </div>
 </template>
 
@@ -12,6 +14,10 @@ export default {
   data() {
     return {
       timer: null,
+      days: null,
+      hours: null,
+      minutes: null,
+      seconds: null,
     };
   },
   created() {
@@ -21,12 +27,41 @@ export default {
     calcTimer: function(){
       var self = this;
       setInterval(function(){
-        var date = new Date();
-        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-          self.timer = date.toLocaleTimeString('pt-BR');
-        } else {
-          self.timer = date.toLocaleTimeString('pt-BR', {timeZone: 'America/Belem'});
-        }
+        // var date = new Date();
+        // if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        //   self.timer = date.toLocaleTimeString('pt-BR');
+        // } else {
+        //   self.timer = date.toLocaleTimeString('pt-BR', {timeZone: 'America/Belem'});
+        // }
+        var data = 'Aug 10, 2019 00:00:00';
+        console.log(data);
+        var second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+        //week = hour * 168;
+
+
+        var s = new Date(data).getTime(),
+        x = setInterval(function() {
+
+          var present = new Date().getTime(),
+          gap = s - present;
+
+          self.days =  Math.floor(gap / (day)),
+          self.hours = ' ' + Math.floor((gap % (day)) / (hour)),
+          self.minutes = ' ' + Math.floor((gap % (hour)) / (minute)),
+          self.seconds = ' ' + Math.floor((gap % (minute)) / second);
+
+
+        });
+
+        var present = new Date().getTime(),
+        gap = s - present;
+        // if ( Math.floor(gap / (day) < 2 ) ) {
+        //   $('#remaining-days-title').text('dia');
+        // }
+
       }, 1000);
     }
   }
