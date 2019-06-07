1ba267f8739253a7bda834c1d557d9811d1708a5
Marco A <mabo.marcoantonio@gmail.com>
2019-06-07 18:34:08 -0300

    Add new file and change main page on website

---
+++ src/components/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Hello World - Welcome to my website - {{ timer }}</h1>
+  </div>
+</template>
+
+<script>
+import { setInterval } from 'timers';
+/* eslint-disable */
+export default {
+  name: 'App',
+  data() {
+    return {
+      timer: null,
+    };
+  },
+  created() {
+    this.calcTimer();
+  },
+  methods:{
+    calcTimer: function(){
+      var self = this;
+      setInterval(function(){
+        var date = new Date();
+        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+          self.timer = date.toLocaleTimeString('pt-BR');
+        } else {
+          self.timer = date.toLocaleTimeString('pt-BR', {timeZone: 'America/Belem'});
+        }
+      }, 1000);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import HelloVueAxios from '@/components/ChamadaAPI-VueAxios';
+import App from '@/components/App';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'App',
+      component: App,
+    },
+    {
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld,
     },
